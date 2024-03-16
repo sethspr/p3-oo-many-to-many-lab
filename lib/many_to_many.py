eb064ec4b543cd71aa268a5e63d30aae21d2f68f
seth spring <seth.c.spring@gmail.com>
2024-03-16 10:28:27 -0400

    3 test passed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,43 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
 
+    def __repr__(self):
+        return f"<Author Name: {self.name})>"
+
+
+    def contracts(self):
+        results = []
+        for contract in Contract.all:
+            if contract.author is self:
+                results.append(contract)
+        return results
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+    
+    def __repr__(self):
+        return f"<Book Title: {self.title})>"
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date=0, royalties=0):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+    def __repr__(self):
+        return f"<Contract Author: {self.author.name}, Book: {self.book.title}, Date: {self.date}, Royalties: {self.royalties})>"
+    
+    
+
+auth_1 = Author("Seth")
+auth_2 = Author("Sammy")
+book_1 = Book("Pythons are Snakes")
+book_2 = Book("Just Say No to JSON")
+contract_1 = Contract(auth_1, book_1)
+
+print(contract_1)
